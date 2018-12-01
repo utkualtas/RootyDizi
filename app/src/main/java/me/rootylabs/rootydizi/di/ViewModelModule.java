9d5eaf61686012769bb8dc85062e6df672b358f7
Utku Altaş <utkualtas96@gmail.com>
2018-12-01 07:02:37 +0300

    Splash screen improved. +LoginFragmentViewModel added.

--- app/src/main/java/me/rootylabs/rootydizi/di/ViewModelModule.java
+++ app/src/main/java/me/rootylabs/rootydizi/di/ViewModelModule.java
@@ -8,6 +8,7 @@ import dagger.Module;
 import dagger.multibindings.IntoMap;
 import me.rootylabs.rootydizi.ui.activities.main.MainActivityViewModel;
 import me.rootylabs.rootydizi.ui.activities.splash.SplashActivityViewModel;
+import me.rootylabs.rootydizi.ui.fragments.splash.LoginFragmentViewModel;
 
 @Module
 public abstract class ViewModelModule {
@@ -21,6 +22,11 @@ public abstract class ViewModelModule {
     @IntoMap
     @Binds
     abstract ViewModel provideSplashViewModel(SplashActivityViewModel splashActivityViewModel);
+
+    @ViewModelKey(LoginFragmentViewModel.class)
+    @IntoMap
+    @Binds
+    abstract ViewModel provideLoginViewModel(LoginFragmentViewModel loginFragmentViewModel);
 
     @Binds
     abstract ViewModelProvider.Factory provideViewModelFactory(ViewModelFactory viewModelFactory);
--- app/src/main/java/me/rootylabs/rootydizi/ui/fragments/splash/LoginFragment.java
+++ app/src/main/java/me/rootylabs/rootydizi/ui/fragments/splash/LoginFragment.java
@@ -1,7 +1,10 @@
 package me.rootylabs.rootydizi.ui.fragments.splash;
 
 import android.app.Activity;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,27 +18,30 @@ import com.thekhaeng.pushdownanim.PushDownAnim;
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import me.rootylabs.rootydizi.R;
 import me.rootylabs.rootydizi.data.api.ApiService;
+import me.rootylabs.rootydizi.data.models.User;
 import me.rootylabs.rootydizi.data.repository.UserRepository;
 import me.rootylabs.rootydizi.databinding.FragmentSplashLoginBinding;
+import me.rootylabs.rootydizi.ui.activities.main.MainActivity;
 import me.rootylabs.rootydizi.utils.SomeUtils;
 import timber.log.Timber;
 
 public class LoginFragment extends DaggerFragment {
 
-    FragmentSplashLoginBinding binding;
     Activity activity;
+    FragmentSplashLoginBinding binding;
+    LoginFragmentViewModel loginFragmentViewModel;
     View view;
     InputMethodManager inputMethodManager;
 
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
     @Inject
     SomeUtils someUtils;
-
     @Inject
     ApiService apiService;
-
     @Inject
     UserRepository userRepository;
 
@@ -43,6 +49,7 @@ public class LoginFragment extends DaggerFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash_login, container, false);
+        loginFragmentViewModel = ViewModelProviders.of(this, viewModelFactory).get(LoginFragmentViewModel.class);
         view = binding.getRoot();
         Init();
         Observe();
@@ -70,24 +77,27 @@ public class LoginFragment extends DaggerFragment {
 
         PushDownAnim.setPushDownAnimTo(binding.fragmentLoginSignIn).setOnClickListener(v -> {
             //Do somethings when click the sign in button.
-
-            //Bu işlem için View Model oluşturmak mı oluşturmamak mı işte bütün mesele bu..
-            userRepository.getUserData("utku", "123456")
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(userContainer -> {
-                        Timber.e(userContainer.getCode());
-                        Timber.e(userContainer.getMessage());
-                        if(userContainer.getCode() == "200"){
-                            Timber.e(userContainer.getData().getName());
-                        }
-                    });
+            loginFragmentViewModel.getLogin(binding.fragmentLoginUsername.getText().toString().trim(), binding.fragmentLoginPassword.getText().toString().trim());
         });
     }
 
     private void Observe() {
-
+        loginFragmentViewModel.getUser().observe(this, this::login);
+        loginFragmentViewModel.getLoginMessage().observe(this, this::loginMessage);
     }
 
+    private void loginMessage(String s) {
+        //Maybe we can show a dialog for login message.
+    }
+
+    private void login(User user) {
+        if(user != null){
+            //Do somethings when if available entered user.
+            Timber.e("Selam " + user.getName());
+            startActivity(new Intent(activity, MainActivity.class));
+            activity.finish();
+        }
+    }
 
     public void clearEditTextsFocus(View v) {
         if (activity.getCurrentFocus() != binding.fragmentLoginFocusable) {
